98277e98b130a9232248dcffd2c9aadc7ea4d059
Martín Díaz <38512764+magadiflo@users.noreply.github.com>
2025-11-27 11:28:03 -0500

    Update job configuration for Java Maven DSL

--- DSL/javamavenDSL2.groovy
+++ DSL/javamavenDSL2.groovy
@@ -1,29 +1,29 @@
-job('Java Maven App DSL 2') {
+job('java-app-con-maven-dsl-v2') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/magadiflo/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('Martín Díaz')
+            node / gitConfigEmail('magadiflo@gmail.com')
         }
     }
     steps {
         maven {
-          mavenInstallation('mavenjenkins')
-          goals('-B -DskipTests clean package')
+            mavenInstallation('Maven en Jenkins')
+            goals('-B -DskipTests clean package')
         }
         maven {
-          mavenInstallation('mavenjenkins')
-          goals('test')
+            mavenInstallation('Maven en Jenkins')
+            goals('test')
         }
         shell('''
           echo "Entrega: Desplegando la aplicación" 
-          java -jar "/var/jenkins_home/workspace/Java Maven App DSL 2/target/my-app-1.0-SNAPSHOT.jar"
-        ''')  
+          java -jar /var/jenkins_home/workspace/java-app-con-maven-dsl/target/my-app-1.0-SNAPSHOT.jar
+        ''')
     }
     publishers {
         archiveArtifacts('target/*.jar')
         archiveJunit('target/surefire-reports/*.xml')
-	slackNotifier {
+        slackNotifier {
             notifyAborted(true)
             notifyEveryFailure(true)
             notifyNotBuilt(false)
@@ -39,6 +39,6 @@ job('Java Maven App DSL 2') {
             commitInfoChoice('NONE')
             teamDomain(null)
             authToken(null)
-       }
+        }
     }
 }
